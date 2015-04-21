ffe4fae222028af176e0738a20828074eb6ff89f
dannychan0510 <dannychan0510>
2015-04-22 00:36:05 +0100

    updated R script

--- clt_simulation.R
+++ clt_simulation.R
@@ -10,8 +10,11 @@ for (i in 1 : 1000) mns = c(mns, mean(rexp(40, lambda)))
 
 # Plotting out a histogram of the distribution
 library(ggplot2)
-mns <- as.data.frame(mns)
-ggplot(mns, aes(mns)) + geom_histogram(binwidth = 0.3, colour = "white", fill = "skyblue4") + labs(y = "Frequency", x = "Mean") + geom_vline(xintercept = 1 / lambda)
+dB <- as.data.frame(mns)
+ggplot(dB, aes(mns)) + geom_histogram(binwidth = 0.3, colour = "white", fill = "skyblue4") + labs(y = "Frequency", x = "Mean") + geom_vline(xintercept = 1 / lambda)
 
 # Exporting as "histogram1.png"
 ggsave(file="histogram1.png")
+
+# Obtaining the sample mean
+mean(mns)
