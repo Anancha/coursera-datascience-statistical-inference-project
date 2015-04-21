d1362c389245a2506a06b1ace59f29d85fa9421c
dannychan0510 <dannychan0510>
2015-04-22 00:01:50 +0100

    updated cli_simulation.R

--- clt_simulation.R
+++ clt_simulation.R
@@ -1,13 +1,17 @@
 > setwd("~/Documents/GitHub/coursera-datascience-statistical-inference-project")
 
-lamda <- 0.2
+# Setting the value of lambda
+lambda <- 0.2
 
-set.seed(100)
+# Obtaining the distribution of the averages of 40 exponentials
+set.seed(123)
 mns = NULL
-for (i in 1 : 1000) mns = c(mns, mean(rexp(40, lamda)))
+for (i in 1 : 1000) mns = c(mns, mean(rexp(40, lambda)))
 
-mean(mns)
-hist(mns)
+# Plotting out a histogram of the distribution
+library(ggplot2)
+mns <- as.data.frame(mns)
+ggplot(mns, aes(mns)) + geom_histogram(binwidth = 0.3, colour = "white", fill = "skyblue4") + labs(y = "Frequency", x = "Mean") + geom_vline(xintercept = 1 / lambda)
 
-
-var(mns)
+# Exporting as "histogram1.png"
+ggsave(file="histogram1.png")
