c75fd147365f49d5c8602049a08f96dfe8780ab1
Danny Chan <dannychan0510@gmail.com>
2015-04-24 16:52:50 +0100

    created part2_statisticalinference.R

--- part2_statisticalinference.R
+++ part2_statisticalinference.R
@@ -3,3 +3,20 @@ setwd("~/Documents/GitHub/coursera-datascience-statistical-inference-project")
 
 # Loading up the Tooth Growth dataset
 ToothGrowth <- ToothGrowth
+
+# Getting information on the dataset
+?ToothGrowth
+
+# Getting a summary of the dataset
+summary(ToothGrowth)
+
+# Doing a chart plot of len by supp and dose
+library (ggplot2)
+ggplot(data = ToothGrowth, aes(x = as.factor(dose), y = len, fill = supp)) +
+  geom_bar(stat = "identity",) +
+  facet_grid(. ~ supp) +
+  xlab("Dosage (in mg)") +
+  ylab("Length of Tooth") +
+  guides(fill = guide_legend(title = "Supplement Type"))
+
+ggsave("toothgrowth.png")
